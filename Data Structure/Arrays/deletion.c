1789f68b5bd0d73e83fc776c151f20a97b725526
Nitin Sharma <nitinsharmaa787@gmail.com>
2023-05-16 12:43:21 +0530

    Array: deletion

--- Data Structure/Arrays/deletion.c
+++ Data Structure/Arrays/deletion.c
@@ -7,31 +7,67 @@ void printArray(int arr[], int size) {
     printf("\n");
 }
 
-
-// maximum n number of shifting
-void deletion(int arr[], int size, int i) {
-    if (i < 0 || i >= size) {
-        printf("Index out of bounds\n");
+// time complexity --Constant-- O(1)
+void delete_last(int arr[], int *length) {
+    if (*length > 0) {
+        *length = *length -1;
+        printArray(arr, *length);
     } else {
-        printf("Array before deletion: ");
-        printArray(arr, size);
-        for (int j = i; j < size - 1; j++) {
-            arr[j] = arr[j+1];
-        }
-        size--;
-        printf("Array after deletion: ");
-        printArray(arr, size);
+        printf("Array is already emptly.\n");
     }
 }
 
+// detele based on index... time complexity O(n)
+void delete_index(int arr[], int index, int length) {
+    if (index < 0 || index > length-1) {
+        printf("Invalid index\n");
+    } else {
+        // index > 0 && length-1)
+        printf("Array before the deletion\n");
+        printArray(arr, length);
+        for (int i = index; i < length-1; i++) {
+            arr[i] = arr[i+1];
+        }
+        length--;
+        printf("Array after the deletion\n");
+        printArray(arr, length);
+    }
+}
+
+// detele in the sorted array... time complexity O(n)
+void delete_sorted(int arr[], int length, int value) {
+    int index = -1;
+    for (int i = 0; i < length; i++) {
+        if (arr[i] == value) {
+            index = i;
+            break;
+        }
+    }
+    if (index == -1) {
+        printf("Item not found in the array\n");
+    } else {
+        printf("Array before the deletion\n");
+        printArray(arr, length);
+        for (int i = index; i < length-1; i++) {
+            arr[i] = arr[i+1];
+        }
+        length--;
+        printf("Array after the deletion\n");
+        printArray(arr, length);
+    }
+}
+
+
 int main() {
     int arr[5] = {1, 2, 3, 4, 5};
-    int size = 5;
+    int length = sizeof(arr)/sizeof(arr[0]);
+    int index;
+    // delete_last(arr, &length);
 
-    // index of the element to be deleted
-    int i = 2;
+    // printf("Enter the index between 0 to %d to delete an element: ",length-1);
+    // scanf("%d", &index);
+    // delete_index(arr, index, length);
 
-    deletion(arr, size, i);
-
+    delete_sorted(arr, length, 4);
     return 0;
 }
