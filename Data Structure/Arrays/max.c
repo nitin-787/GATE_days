4644033678f7f592947a6009231ad4d2ba24bc92
Nitin Sharma <nitinsharmaa787@gmail.com>
2023-05-16 19:27:21 +0530

    Array: Min & Max

--- Data Structure/Arrays/max.c
+++ Data Structure/Arrays/max.c
@@ -1,23 +1,55 @@
 #include <stdio.h>
 
-int find_min(int arr[], int n)  {
+// regular method --time complexity-- theta(n)
+// space complexity --constant-- theta(1)
+// no. of comparison = n-1
+int find_max(int arr[], int n)  {
     int max = arr[0];
+    int j = 0;
     for (int i = 0; i < n; i++) {
         if (arr[i] > max) {
             max = arr[i];
+            j = i;
         }
-        printf("min: %d\n", max); // updating the minimun
     }
-    return max;
+    printf("Maximum element is: %d\n", max); 
+    return j;
+}
+
+// tournament method --time complexity-- theta(n)
+// no. of comparison = n-1
+// space complexity = O(n)
+int find_max_tournament(int arr[], int n) {
+    int arr1[n];
+    int i,j;
+    // copying the elements of arr in arr1
+    for (i = 0; i < n; i++) {
+        arr1[i] = arr[i];
+    }
+    while (n > 1) {
+        j=0;
+        for (i = 0; i < n-1; i+=2) {
+            if (arr1[i] > arr1[i+1])
+                arr1[j] = arr1[i];
+            else
+                arr1[j] = arr1[i+1];
+            j++;
+        } 
+        if (i == n-1)
+            arr1[j] = arr1[i];
+        n = (n+1)/2;
+    }
+    return arr1[0];
 }
 
 void main() {
     int arr[] = {15,8,9,7,19,20,4,21,11,6,15};
     int n = sizeof(arr) / sizeof(arr[0]);
     
-    int max = find_min(arr, n);
-    printf("Maximun element is: %d\n", max);
+    // int max = find_max(arr, n);
+    // printf("Maximun element is present at index: %d\n", max);
 
-    printf("Length of array: %d\n", n);
+    int min = find_max_tournament(arr, n);
+    printf("%d", min);
 }
 
--- Data Structure/Arrays/min.c
+++ Data Structure/Arrays/min.c
@@ -1,23 +1,56 @@
 #include <stdio.h>
 
+
+// regular method --time complexity-- theta(n)
+// space complexity --constant-- theta(1)
+// no. of comparison = n-1
 int find_min(int arr[], int n)  {
     int min = arr[0];
+    int j = 0;
     for (int i = 0; i < n; i++) {
         if (arr[i] < min) {
             min = arr[i];
+            j=i;
         }
-        printf("min: %d\n", min); // updating the minimun
     }
-    return min;
+    printf("Minimum elemtent in array is: %d\n", min); 
+    return j;
+}
+
+// tournament method --time complexity-- theta(n)
+// no. of comparison = n-1
+// space complexity = O(n)
+int find_min_tournament(int arr[], int n) {
+    int arr1[n];
+    int i,j;
+    // copying the elements of arr in arr1
+    for (i = 0; i < n; i++) {
+        arr1[i] = arr[i];
+    }
+    while (n > 1) {
+        j=0;
+        for (i = 0; i < n-1; i+=2) {
+            if (arr1[i] < arr1[i+1])
+                arr1[j] = arr1[i];
+            else
+                arr1[j] = arr1[i+1];
+            j++;
+        } 
+        if (i == n-1)
+            arr1[j] = arr1[i];
+        n = (n+1)/2;
+    }
+    return arr1[0];
 }
 
 void main() {
     int arr[] = {15,8,9,7,19,20,4,21,11,6,15};
     int n = sizeof(arr) / sizeof(arr[0]);
     
-    int min = find_min(arr, n);
-    printf("Minimum element is: %d\n", min);
+    // int min = find_min(arr, n);
+    // printf("Minimum is present at index: %d\n", min);
 
-    printf("Length of array: %d\n", n);
+    int min = find_min_tournament(arr, n);
+    printf("%d", min);
 }
 
