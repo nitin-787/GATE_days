bbc4728ca2ebfaaea7e7af3658d3c0372b55a266
Nitin Sharma <nitinsharmaa787@gmail.com>
2023-05-21 17:32:15 +0530

    Arrays: complete

--- Data Structure/Arrays/min_max_withMinComp.c
+++ Data Structure/Arrays/min_max_withMinComp.c
@@ -3,51 +3,48 @@
 
 
 // no. of comparisons are  3*n/2 - 2
+// time complexity -- theta of n
+// space complexity --constant-- theta(1)
 void findMinMax1(int arr[], int n, int* minVal, int* maxVal) {
+    *minVal = arr[0];
+    *maxVal = arr[0];
     int i;
-    if (n % 2 == 0) {
-        if (arr[0] > arr[1]) {
-            *maxVal = arr[0];
-            *minVal = arr[1];
+    for (i = 0; i < n-1; i+=2) {
+        if (arr[i] < arr[i+1]) {
+            if (arr[i] < *minVal) {
+                *minVal = arr[i];
+            } 
+            if (arr[i+i] > *maxVal) {
+                *maxVal = arr[i+1];
+            }
         } else {
-            *maxVal = arr[1];
-            *minVal = arr[0];
-        }
-        i = 2;
-    } else {
-        *maxVal = arr[0];
-        *minVal = arr[0];
-        i = 1;
-    }
-
-    while (i < n - 1) {
-        if (arr[i] > arr[i+1]) {
+            if(arr[i+1] < *minVal) {
+                *minVal = arr[i+1];
+            }
             if (arr[i] > *maxVal) {
                 *maxVal = arr[i];
             }
-            if (arr[i+1] < *minVal) {
-                *minVal = arr[i+1];
-            }
-        } else {
-            if (arr[i+1] > *maxVal) {
-                *maxVal = arr[i+1];
-            }
-            if (arr[i] < *minVal) {
-                *minVal = arr[i];
-            }
         }
-        i += 2;
     }
-
-    printf("Number of comparisons: %d\n", 3*n/2 - 2);
+    if (n % 2 == 1) {
+        if (arr[n-1] < *minVal) {
+            *minVal = arr[n-1];
+        }
+        if (arr[n-1] > *maxVal) {
+            *maxVal = arr[n-1];
+        }
+    }
+    printf("Minimum = %d\n", *minVal);
+    printf("Maximum = %d\n", *maxVal);
 }
 
 
 // no. of comparisons are  2n - 2
+// time complexity -- theta of n
+// space complexity --constant-- theta(1)
 void findMinMax(int arr[], int n, int* minVal, int* maxVal) {
     if (n == 0) {
-        *minVal = INT_MAX;
-        *maxVal = INT_MIN;
+        printf("Array is empty");
         return;
     }
     int i;
@@ -63,18 +60,18 @@ void findMinMax(int arr[], int n, int* minVal, int* maxVal) {
         }
     }
     printf("Number of comparisons: %d\n", numComparisons);
+    printf("Minimum value: %d\n", minVal);
+    printf("Maximum value: %d\n", maxVal);
 }
 
 int main() {
     int arr[] = {5, 3, 1, 9, 8};
     int n = sizeof(arr) / sizeof(arr[0]);
     int minVal, maxVal;
-    findMinMax(arr, n, &minVal, &maxVal);
+
+    // findMinMax(arr, n, &minVal, &maxVal);
 
     findMinMax1(arr, n, &minVal, &maxVal);
-
-    printf("Minimum value: %d\n", minVal);
-    printf("Maximum value: %d\n", maxVal);
     return 0;
 }
 
